253ed623a4d3866cc37946d3cdf1bdf4a7fc87ed
Boluo-Baba <1040239294@qq.com>
2025-07-16 20:59:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import faicons as fa
 import plotly.express as px
 import plotly.graph_objs as go
+import plotly.tools as tls
 import pandas as pd
 import numpy as np
 
@@ -416,24 +417,16 @@ with ui.layout_columns(col_widths=[6], fill=False):
             # z = np.random.randn(10, 12)
             # fig = px.imshow(z, color_continuous_scale="Viridis")
             # return fig
+            
+            fig, ax = plt.subplots(figsize=(6, 4))
+            plt.imshow(a.data.iloc[10: -9, 8: -10], cmap=cmap, interpolation='nearest', vmin=32, vmax=47,)
+            plt.xticks(np.arange(0,121,20), np.arange(-6, 7, 2))
+            plt.yticks(np.arange(0,121,20), np.arange(-6, 7, 2))
+            plt.grid(True, color='grey', linestyle='--', alpha=0.5)
+            plt.colorbar()
 
-            return go.Figure(
-                    data=go.Heatmap(
-                        z=a.data.iloc[10: -9, 8: -10],
-                        zmin=32,
-                        zmax=47,
-                        colorscale=cmap
-                    )
-                )
-            
-            # fig, ax = plt.subplots(figsize=(6, 4))
-            # plt.imshow(a.data.iloc[10: -9, 8: -10], cmap=cmap, interpolation='nearest', vmin=32, vmax=47,)
-            # plt.xticks(np.arange(0,121,20), np.arange(-6, 7, 2))
-            # plt.yticks(np.arange(0,121,20), np.arange(-6, 7, 2))
-            # plt.grid(True, color='grey', linestyle='--', alpha=0.5)
-            # plt.colorbar()
-            
-            return fig
+            fig_plotly = tls.mpl_to_plotly(fig)
+            return fig_plotly
             
             # return px.scatter(
             #     tips_data(),
